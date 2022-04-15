06d3e61feb056b0c09a3365ec958ae2baf46e31f
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2022-04-15 07:42:05 +0700

    fix

---
+++ src/data/products.js
@@ -0,0 +1,53 @@
+let products=[
+    {
+      // _id: ObjectId("6253a7f0e5259fee8fdf906d"),
+      name: 'kaos malioboro jogja',
+      harga: 60000,
+      category: 'kaos',
+      image: '',
+      view: 0
+    },
+    {
+      // _id: ObjectId("6253a9fd4a1ba010f43b34ad"),
+      name: 'kaos sleman',
+      harga: 50000,
+      category: 'kaos',
+      image: '',
+      view: 0
+    },
+    {
+      // _id: ObjectId("6253aa0d4a1ba010f43b34ae"),
+      name: 'kaos bantul',
+      harga: 60000,
+      category: 'kaos',
+      image: '',
+      view: 0
+    },
+    {
+      // _id: ObjectId("6253aa4e4a1ba010f43b34af"),
+      name: 'gantungan kunci love',
+      harga: 20000,
+      category: 'gantungan kunci',
+      image: '',
+      view: 0
+    },
+    {
+      // _id: ObjectId("6253e9bb2c5691c8b082dae4"),
+      name: 'sandal kesehatan merk sumino',
+      harga: 20000,
+      category: 'sandal',
+      image: '',
+      view: 0
+    },
+    {
+      // _id: ObjectId("6253e9e62c5691c8b082dae5"),
+      name: 'sandal kesehatan merk sumino large',
+      harga: 25000,
+      category: 'sandal',
+      image: '',
+      view: 0
+    }
+  ]
+
+ 
+  
---
+++ src/view/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+//import components
+// import Header from "../component/Header";
+// import Footer from "../component/Footer";
+// import Hero from "../component/homeComponents/Hero";
+import ShopSection from "../component/homeComponents/ShopSection";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <ShopSection />
+
+            </div>
+        );
+    }
+}
+
+
+
+export default Home;
