f4144c752f729d7f00fcbc886b24657488e0921a
akhmadrma <20523049@students.uii.ac.id>
2022-04-27 21:50:43 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from '../src/component/Header/Header'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Home from '../src/view/Home'
-import second from '../src/component/Footer'
 import Footer from '../src/component/Footer';
 
 function App() {
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react'
 import {Navbar,NavDropdown, Nav, Container } from "react-bootstrap";
+import Logo from '../../images/Logo.png'
 
 export class Header extends Component {
   render() {
     return (
-<Navbar bg="light" expand="lg">
+      
+<Navbar collapseOnSelect expand="lg" bg="primary" variant="light" className='shadow-lg shadow' >
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar.Brand href="#home">
+  <img
+        src={Logo}
+        width="100"
+        height=""
+        className="d-inline-block align-top"
+        alt="Mstore"
+      />
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#home" id='home'>Home</Nav.Link>
+      <NavDropdown title="Product" id="collasible-nav-dropdown">
+        <NavDropdown.Item  href="#action/3.1"><img src={Logo} width='100'></img></NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2"><img src={Logo} width='100'></img></NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3"><img src={Logo} width='100'></img></NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#pricing">About</Nav.Link>
+      <Nav.Link href="#pricing">Contact</Nav.Link>
+
+      
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
   </Container>
 </Navbar>
     )
